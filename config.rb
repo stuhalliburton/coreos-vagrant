cc1b2e742726eac8c39dd70f961e8aebaa9d43cc
Stu Halliburton <stu.halliburton@gmail.com>
2016-03-23 22:21:29 +0000

    Config: set cluster reboot stratey -> etcd-lock

--- config.rb
+++ config.rb
@@ -22,12 +22,8 @@ if File.exists?('user-data') && ARGV[0].eql?('up')
     data['coreos']['etcd2']['discovery'] = token
   end
 
-  # Fix for YAML.load() converting reboot-strategy from 'off' to `false`
-  if data.key? 'coreos' and data['coreos'].key? 'update' and data['coreos']['update'].key? 'reboot-strategy'
-    if data['coreos']['update']['reboot-strategy'] == false
-      data['coreos']['update']['reboot-strategy'] = 'off'
-    end
-  end
+  # Set reboot strategy
+  (data['coreos']['update'] ||= {}) && (data['coreos']['update']['reboot-strategy'] = 'etcd-lock')
 
   yaml = YAML.dump(data)
   File.open('user-data', 'w') { |file| file.write("#cloud-config\n\n#{yaml}") }
